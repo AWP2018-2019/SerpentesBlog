f06aa664f3fd8fb87e6ddc08159556aadc176e1b
Ioana Ionita <ioana_ionita97@yahoo.com>
2019-01-11 19:27:13 +0000

    First commit

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    text = models.CharField(max_length=200)
+    profile_id = models.ForeignKey(UserProfile, related_name='profile')
+    created_by = models.ForeignKey(User, related_name='posts')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{} created by {} at {}".format(self.text, self.created_by.username, self.created_at)
+        
--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,5 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from models import Post
 # Create your views here.
