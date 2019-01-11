e61107b8fdfed9c8258cf177548ad7ae09296f82
Ioana Ionita <ioana_ionita97@yahoo.com>
2019-01-11 20:37:20 +0000

    Comment create view

--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,12 @@ from django.conf.urls import url
 from views import (
     index,
     PostListView,
+    CommentCreateView,
+    post_detail,
 )
 
 urlpatterns = [
     url(r'^$', index, name='index'),
     url(r'^$', PostListView.as_view(), name='post_list'),
+    url(r'^post/(?P<pk>[0-9]+)/comment/create$', CommentCreateView.as_view(), name='comment_create'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,12 @@ from django.shortcuts import render
 
 from django.views import View
 from django.views.generic import TemplateView, ListView, DetailView
-from models import Post
+from models import Post, Comment
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 # Create your views here.
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 def index(request):
     post_list = Post.objects.all()
@@ -16,3 +20,16 @@ class PostListView(ListView):
     template_name = 'index.html'
     model = Post
     context_object_name = 'post_list'
+    
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    fields = ['text']
+    
+    def form_valid(self, form):
+        post = Post.objects.get(id=self.kwargs['pk'])
+        Comment.objects.create(
+            created_by=self.request.user,
+            post=post,
+            **form.cleaned_data
+        )
+        return redirect(reverse_lazy("post_detail", kwargs={"pk": self.kwargs['pk']}))
