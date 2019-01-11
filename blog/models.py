ed1f043247e7a17da38a8a06ab21a009e04bcf9e
Mihaela Versin <mihaelaversin@gmail.com>
2019-01-11 19:37:38 +0000

    First commit

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth import get_user_model
 from django.db import models
+
+User = get_user_model()
 
 # Create your models here.
 class Post(models.Model):
@@ -14,3 +17,10 @@ class Post(models.Model):
     def __str__(self):
         return "{} created by {} at {}".format(self.text, self.created_by.username, self.created_at)
         
+
+class Image(models.Model):
+    nume = models.CharField(max_length=200)
+    image = models.FileField()
+    post = models.ForeignKey(Post, related_name="images")
+    added_at = models.DateTimeField(auto_now_add=True)
+    added_by = models.ForeignKey(User, related_name='images')
